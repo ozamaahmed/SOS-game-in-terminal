e500ff0e3114c52772fe747cacc4cfc9c1754540
ozamaahmed <93034776+ozamaahmed@users.noreply.github.com>
2023-08-27 03:50:01 +0530

    proper working SOS

--- sos.py
+++ sos.py
@@ -3,8 +3,6 @@
 # modify this mfing code!!!! i forgot to add the checking conditions if the current letter is O in only did that for S so do that me
 
 
-from collections import deque
-
 def SpaceLeft(board): 
     for i in board:
         for j in i:
@@ -84,12 +82,12 @@ def checkS(x,y,turn):
         marked.add(((i-2,j-2),(i-1,j-1),(i,j)))
         c+=1
     # top up
-    if ((i,j),(i-1,j),(i-2,j)) not in marked and checkings[0]!=-1 and board[i][j] == "S" and board[i-1][j] == "O" and board[i-2][j] == "S":
+    if ((i,j),(i-1,j),(i-2,j)) not in marked and checkings[1]!=-1 and board[i][j] == "S" and board[i-1][j] == "O" and board[i-2][j] == "S":
         marked.add(((i,j),(i-1,j),(i-2,j)))
         marked.add(((i-2,j),(i-1,j),(i,j)))
         c+=1
     # top right
-    if ((i,j),(i-1,j+1),(i-2,j+2)) not in marked and checkings[0]!=-1 and board[i][j] == "S" and board[i-1][j+1] == "O" and board[i-2][j+2] == "S":
+    if ((i,j),(i-1,j+1),(i-2,j+2)) not in marked and checkings[2]!=-1 and board[i][j] == "S" and board[i-1][j+1] == "O" and board[i-2][j+2] == "S": 
         marked.add(((i,j),(i-1,j+1),(i-2,j+2)))
         marked.add(((i-2,j+2),(i-1,j+1),(i,j)))
         c+=1
@@ -104,7 +102,7 @@ def checkS(x,y,turn):
         marked.add(((i+2,j+2),(i+1,j+1),(i,j)))
         c+=1
     # top down
-    if ((i,j),(i+1,j),(i+2,j)) not in marked and checkings[1]!=-1 and board[i][j] == "S" and board[i+1][j] == "O" and board[i+2][j] == "S":
+    if ((i,j),(i+1,j),(i+2,j)) not in marked and checkings[5]!=-1 and board[i][j] == "S" and board[i+1][j] == "O" and board[i+2][j] == "S":
         marked.add(((i,j),(i+1,j),(i+2,j)))
         marked.add(((i+2,j),(i+1,j),(i,j)))
         c+=1
@@ -118,6 +116,7 @@ def checkS(x,y,turn):
         marked.add(((i,j),(i+1,j-1),(i+2,j-2)))
         marked.add(((i+2,j-2),(i+1,j-1),(i,j)))
         c+=1
+
 
     if turn == 1: 
         player1[0]+=c 
@@ -133,42 +132,58 @@ def checkO(x,y,turn):
     c = 0
     i,j = x,y
 
-    top,bottom,right,left = True,True,True,True 
+    vertical,cross,horizontal = True,True,True 
     # if the O is in corners
     if x == 0 or x == len(board)-1:
-        right = False
+        vertical = False
+        cross = False
     if y == 0 or y == len(board)-1:
-        top = False 
+        horizontal = False 
+        cross = False
 
-    checkings = [0 for i in range(4)] 
-    if not top:
+    checkings = [0 for i in range(3)] 
+    if not horizontal:
         checkings[0] = -1
-    if not right:
+    if not cross:
         checkings[1] = -1 
+    if not vertical:
+        checkings[2] = -1
 
 
-    #checking corners
-    # top down check
-    if checkings[0]!=-1 and ((i-1,j),(i,j),(i+1,j)) not in marked and board[i-1][j] == "S" and board[i][j] == "O" and board[i][j+1] == "S":
-        marked.add(((i-1,j),(i,j),(i+1,j))) 
-        marked.add(((i+1,j+1),(i,j),(i-1,j))) 
-        c+=1
-    # left to right check
-    if checkings[0]!=-1 and ((i,j),(i,j),(i,j)) not in marked and board[i][j] == "S" and board[i][j] == "O" and board[i][j] == "S":
+    #checking corners now 
+    # vertical check 
+    if checkings[0]!=-1 and ((i,j-1),(i,j),(i,j+1)) not in marked and board[i][j-1] == "S" and board[i][j] == "O" and board[i][j+1] == "S":
+        marked.add(((i,j-1),(i,j),(i,j+1))) 
+        marked.add(((i,j+1),(i,j),(i,j-1))) 
+        c+=1 
+    # cross checks 
+    #we need to do two cross checks 
+    if checkings[1]!=-1 and (((i-1,j-1),(i,j),(i+1,j+1)) not in marked and board[i-1][j-1] == "S" and board[i][j] == "O" and board[i+1][j+1] == "S"):
         marked.add(((i,j),(i,j),(i,j))) 
         marked.add(((i,j),(i,j),(i,j))) 
         c+=1
-    # top left to bottom right
-    if checkings[0]!=-1 and ((i,j),(i,j),(i,j)) not in marked and board[i][j] == "S" and board[i][j] == "O" and board[i][j] == "S":
-        marked.add(((i,j),(i,j),(i,j))) 
-        marked.add(((i,j),(i,j),(i,j))) 
+
+    if checkings[1]!=-1 and (((i+1,j-1),(i,j),(i-1,j+1)) not in marked and board[i+1][j-1] == "S" and board[i][j] == "O" and board[i-1][j+1] == "S"):
+        marked.add(((i+1,j-1),(i,j),(i-1,j+1))) 
+        marked.add(((i-1,j+1),(i,j),(i+1,j-1))) 
         c+=1
     
-    # top right to bottom left
-    if checkings[0]!=-1 and ((i,j),(i,j),(i,j)) not in marked and board[i][j] == "S" and board[i][j] == "O" and board[i][j] == "S":
-        marked.add(((i,j),(i,j),(i,j))) 
-        marked.add(((i,j),(i,j),(i,j))) 
-        c+=1
+    # top checks 
+    if checkings[2]!=-1 and ((i-1,j),(i,j),(i+1,j)) not in marked and board[i-1][j] == "S" and board[i][j] == "O" and board[i+1][j] == "S":
+        marked.add(((i-1,j),(i,j),(i+1,j))) 
+        marked.add(((i+1,j),(i,j),(i-1,j))) 
+        c+=1 
+    
+    
+    if turn == 1: 
+        player1[0]+=c 
+    else: 
+        player2[0]+=c 
+
+    if c>0:
+        return 1
+    else:
+        return 0
 
 
 
@@ -179,30 +194,46 @@ player2 = [0]
 size = int(input("Enter the number of rows and columns: "))
 board = [["-" for i in range(size)] for j in range(size)]
 
-print("Welcome to SOS game there are two players, p1 goes first and p2 goes next. select (X Y S/O) in this format to insert. enter exit to exit") 
+print("Welcome to SOS game there are two players, p1 goes first and p2 goes next. select (X Y S/O) in this format to insert. enter E to exit") 
 
 turn = 1
 visited = set([])
 while SpaceLeft(board):
     print(f"player {turn} turn: ")
-    inp = input("type exit to exit else press any key.")
-    if inp == "exit":
-        #show winner and loser and points and bye
+
+    position = input("enter the X and Y: ")
+    if position == "exit":
         display(board)
         scores()
         final()
         break
-    else: 
-        row,col= map(int,input("enter row and col: ").split())
+    else:
+        row,col = map(int,position.split())
+    
 
     so = input("S/O: ") 
     #if no so is there insert and check 
     if (row,col) in visited:
         print("element already there choose other place!")
+        continue
     else:
-        board[row][col] = so
-        point = checkS(row,col,turn)
+        if so in "Ss":
+            board[row][col] = "S"
+            point = checkS(row,col,turn)
+        elif so in "Oo":
+            board[row][col] = "O"
+            point = checkO(row,col,turn)
+        elif so == "exit":
+            display(board)
+            scores()
+            final()
+            break
+        else:
+            print("you can only choose S or O! ")
+            continue
+        
         display(board)
+        print(f"placed {so} at {row} {col}")
         scores()
         if point == 1:
             continue
